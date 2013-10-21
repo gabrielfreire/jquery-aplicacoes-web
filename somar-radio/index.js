56ffc6b3c977ad1a87835329bdd7301ef0b0634c
Gabriel Freire <gabrielfreire14@hotmail.com>
2013-10-21 00:34:39 -0200

    terminei exemplo

---
+++ somar-radio/index.js
@@ -0,0 +1,21 @@
+
+
+// quando o input radio for clicado...
+$("input.radiobox").on("click", function (){
+   
+    var soma = 0;                //declarar variável de soma
+    var radio = $(this).val();   //resgatar valor desta radio que foi selecionada
+    
+    //converter a radio para inteiro(pois ela vem como string) e somar com 100
+    soma = parseInt(radio) + 100; 
+    
+    $("label")
+        .hide()              //vou esconder a label
+            .text(soma)      //vou inserir o valor da soma
+                .fadeIn();   //vou mostrar na tela com o novo valor
+    
+    //exibir no console, instale o plugin do firebug(navegador firefox) ao trabalhar com jquery
+    console.log(soma);
+});
+
+
---
+++ somar-radio/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <center>
+            <h2>Exemplo para somar input radio</h2>        
+
+            <p><input type="radio" name="radio" class="radiobox" value="10,00" />10,00</p>
+            <p><input type="radio" name="radio" class="radiobox" value="20,00" />20,00</p>
+            <p>Valor total = <label>100</label></p>
+            
+        </center>
+    </body>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="index.js" type="text/javascript"></script>
+</html>
